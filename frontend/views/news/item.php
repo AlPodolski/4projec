475fcd5f6604cdbb6bde85a0eb941ebfd4668fc2
sanya <sanyapodolski@gmail.com>
2020-02-14 16:00:52 +0200

    вывод новостей и ajax подгрузка при скроле

--- frontend/views/news/item.php
+++ frontend/views/news/item.php
@@ -6,7 +6,7 @@ use common\models\News;
 <div class="row advert-item">
     <div class="col-12">
         <div class="name">
-            <h5><?php echo $news->title; ?></h5>
+            <?php echo $news->title; ?>
         </div>
         <div class="text-ab">
             <?php echo $news->text; ?>
