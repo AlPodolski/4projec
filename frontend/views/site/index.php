7e27ac2949acf379a9083221e3499cc6b090b1f4
sanya <sanyapodolski@gmail.com>
2020-02-18 16:32:41 +0200

    Верстка банера на главной

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -22,6 +22,94 @@ $this->title = 'Знакомства';
                 echo SidebarWidget::Widget()
 
             ?>
+
+            <div class="col-9 main-banner-wrap">
+                <div class="main-banner">
+                    <div class="row">
+                        <div class="col-12">
+                            <h1>Секс знакомства Москва</h1>
+                            <div class="text-main">
+                                <p>Привет мой похотливый друг! Мы рады видеть тебя на сайте Intim DOSUG, здесь ты найдешь
+                                    для себя самые интересные анкеты рыба текст рыба текст рыба текст рыба текст рыба
+                                    текст рыба текст рыба текст рыба текст рыба текст рыба текст рыба текст рыба текст
+                                    рыба текст рыба текст рыба текст рыба текст рыба текст рыба текст рыба текст рыба
+                                    текст рыба текст рыба текст рыба текст рыба текст рыба текст рыба текст рыба текст
+                                    рыба текст рыба текст </p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="row">
+                        <div class="col-12">
+                            <h5>Выберите раздел для поиска секса:</h5>
+                        </div>
+                        <div class="col-6 main-banner-item">
+                            <div class="row">
+                                <div class="col-5">
+                                    <img class="baner-menu-img" src="/files/img/dandy.png" alt="">
+                                </div>
+                                <div class="col-7">
+                                    <ul class="banner-menu-list">
+                                        <li><a href="">Ищу мужчину</a></li>
+                                        <li><a href="">Ищу любовника</a></li>
+                                        <li><a href="">Ищу гея</a></li>
+                                        <li><a href="">Ищу спонсора</a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-6 main-banner-item">
+                            <div class="row">
+                                <div class="col-5">
+                                    <img class="baner-menu-img" src="/files/img/canary.png" alt="">
+                                </div>
+                                <div class="col-7">
+                                    <ul class="banner-menu-list">
+                                        <li><a href="">Ищу женщину</a></li>
+                                        <li><a href="">Ищу любовницу</a></li>
+                                        <li><a href="">Ищу лесбиянку</a></li>
+                                        <li><a href="">Ищу спонсора</a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-6 main-banner-item">
+                            <div class="row">
+                                <div class="col-5">
+                                    <img class="baner-menu-img" src="/files/img/catwoman.png" alt="">
+                                </div>
+                                <div class="col-7">
+                                    <ul class="banner-menu-list">
+                                        <li><a href="">Ищу BDSM</a></li>
+                                        <li><a href="">Ищу госпажу</a></li>
+                                        <li><a href="">Ищу рабыню</a></li>
+                                        <li><a href="">Ищу госпадина</a></li>
+                                        <li><a href="">Ищу раба</a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-6 main-banner-item">
+                            <div class="row">
+                                <div class="col-5">
+                                    <img class="baner-menu-img" src="/files/img/nick.png" alt="">
+                                </div>
+                                <div class="col-7">
+                                    <ul class="banner-menu-list">
+                                        <li><a href="">Ищу проститутку</a></li>
+                                        <li><a href="">Ищу массажистку</a></li>
+                                        <li><a href="">Ищу стриптизершу</a></li>
+                                        <li><a href="">Ищу эскорт девушку</a></li>
+                                        <li><a href="">Ищу жигало</a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+
         </div>
 
     </div>
