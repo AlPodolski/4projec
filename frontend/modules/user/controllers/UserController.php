89f3880dfc4ce29ba8881d4db85ccb2ac59efa20
sanya <sanyapodolski@gmail.com>
2020-03-17 13:55:52 +0200

    Кеширование получения поста

--- frontend/modules/user/controllers/UserController.php
+++ frontend/modules/user/controllers/UserController.php
@@ -1,7 +1,9 @@
 <?php
 
 namespace frontend\modules\user\controllers;
+use backend\models\MetaTemplate;
 use frontend\modules\user\models\Profile;
+use Yii;
 
 
 class UserController extends \yii\web\Controller
@@ -20,31 +22,39 @@ class UserController extends \yii\web\Controller
 
     public function actionView($city, $id){
 
-        $model = Profile::find()->where(['id' => $id])
-            ->with('place')
-            ->with('sexual')
-            ->with('bodyType')
-            ->with('national')
-            ->with('financialSituation')
-            ->with('interesting')
-            ->with('professionals')
-            ->with('vneshnost')
-            ->with('children')
-            ->with('family')
-            ->with('wantFind')
-            ->with('celiZnakomstvamstva')
-            ->with('haracter')
-            ->with('lifeGoals')
-            ->with('smoking')
-            ->with('alcogol')
-            ->with('education')
-            ->with('breast')
-            ->with('intimHair')
-            ->with('hairColor')
-            ->with('sferaDeyatelnosti')
-            ->with('zhile')
-            ->with('transport')
-            ->one();
+        $model = Yii::$app->cache->get('4dosug_profile_data_'.$id);
+
+        if ($model === false) {
+            // $data нет в кэше, вычисляем заново
+            $model = Profile::find()->where(['id' => $id])
+                ->with('place')
+                ->with('sexual')
+                ->with('bodyType')
+                ->with('national')
+                ->with('financialSituation')
+                ->with('interesting')
+                ->with('professionals')
+                ->with('vneshnost')
+                ->with('children')
+                ->with('family')
+                ->with('wantFind')
+                ->with('celiZnakomstvamstva')
+                ->with('haracter')
+                ->with('lifeGoals')
+                ->with('smoking')
+                ->with('alcogol')
+                ->with('education')
+                ->with('breast')
+                ->with('intimHair')
+                ->with('hairColor')
+                ->with('sferaDeyatelnosti')
+                ->with('zhile')
+                ->with('transport')
+                ->one();
+            // Сохраняем значение $data в кэше. Данные можно получить в следующий раз.
+            Yii::$app->cache->set('4dosug_profile_data_'.$id, $model);
+        }
+
 
         return $this->render('single' , [
             'model' => $model
