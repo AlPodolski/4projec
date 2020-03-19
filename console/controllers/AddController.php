253536199a0ef7ed9a0b53985b4adc9c2e9d2290
sanya <sanyapodolski@gmail.com>
2020-03-19 10:06:35 +0200

    обавление описания для женщин

--- console/controllers/AddController.php
+++ console/controllers/AddController.php
@@ -12,9 +12,9 @@ class AddController extends Controller
 {
     public function actionAddDescription()
     {
-        $user_ids = ArrayHelper::getColumn(UserPol::find()->where(['pol_id' => 2])->select('user_id')->asArray()->all(), 'user_id');
+        $user_ids = ArrayHelper::getColumn(UserPol::find()->where(['pol_id' => 1])->select('user_id')->asArray()->all(), 'user_id');
 
-        $users_with_not_text = Profile::find()->where(['text' => null])->andWhere(['in', 'id', $user_ids])->all();
+        $users_with_not_text = Profile::find()->where(['text' => ''])->andWhere(['in', 'id', $user_ids])->all();
 
         $users_with_text = ArrayHelper::getColumn(Profile::find()->where(['!=', 'text', ''])->andWhere(['in', 'id', $user_ids])->select('text')->asArray()->all(), 'text');
 
