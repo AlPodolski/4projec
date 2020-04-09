08565a019a8dbb00c9d1bcf9c27b6623c292a354
sanya <sanyapodolski@gmail.com>
2020-04-09 13:58:09 +0300

    заливка цвета волос

--- console/controllers/AddController.php
+++ console/controllers/AddController.php
@@ -6,6 +6,7 @@ namespace console\controllers;
 use common\models\City;
 use common\models\User;
 use frontend\models\relation\UserZnakomstva;
+use frontend\models\UserHairColor;
 use frontend\models\UserPol;
 use frontend\models\UserProstitutki;
 use frontend\modules\user\models\Profile;
@@ -34,23 +35,22 @@ class AddController extends Controller
 
     public function actionAddToZnakom()
     {
-        $profilesPr = UserProstitutki::find()->select('user_id')->asArray()->all();
 
-        $users = Profile::find()->where(['not in', 'id', ArrayHelper::getColumn($profilesPr, 'user_id') ])->asArray()->all();
+        $users = Profile::find()->asArray()->all();
 
         echo 1;
 
         foreach ($users as $user){
 
 
-            if (!UserZnakomstva::find()->where(['user_id' => $user['id']])->asArray()->all()){
+            if (!UserHairColor::find()->where(['user_id' => $user['id']])->asArray()->all()){
 
-                $userZn = new UserZnakomstva();
+                $userZn = new UserHairColor();
 
                 $userZn->user_id=  $user['id'];
 
                 $userZn->city_id = ArrayHelper::getValue(City::find()->where(['url' => $user['city']])->asArray()->one(), 'id');
-                $userZn->param_id = 1;
+                $userZn->value = \rand(1, 4);
 
                 $userZn->save();
 
