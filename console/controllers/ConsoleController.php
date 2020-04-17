2c4726f7434d3b7b6c0c5d1e67987b5c1ad972e1
sanya <sanyapodolski@gmail.com>
2020-04-17 14:12:05 +0300

    удаление пользователей с длинными иминами

--- console/controllers/ConsoleController.php
+++ console/controllers/ConsoleController.php
@@ -3,9 +3,14 @@
 
 namespace console\controllers;
 
+use common\models\FilterParams;
 use frontend\modules\user\components\helpers\FriendsHelper;
+use frontend\modules\user\components\helpers\SaveAnketInfoHelper;
 use frontend\modules\user\models\Friends;
+use frontend\modules\user\models\Photo;
 use frontend\modules\user\models\Profile;
+use frontend\modules\wall\models\Wall;
+use Yii;
 use yii\console\Controller;
 
 class ConsoleController extends Controller
@@ -44,6 +49,55 @@ class ConsoleController extends Controller
                     }
 
                 }
+            }
+
+        }
+    }
+
+    public function actionRemoveUsers()
+    {
+
+        $path = \str_replace('console', 'frontend',Yii::getAlias('@app') );
+
+        $profiles = Profile::find()->asArray()->all();
+
+        foreach ($profiles as $profile){
+
+            if(substr_count($profile['username'], ' ') > 2){
+
+                Wall::deleteAll(['from' => $profile['id']]);
+
+                if ($friends = \frontend\modules\user\components\Friends::getFriendsIds($profile['id'])){
+
+                    foreach ($friends as $friend){
+
+                        FriendsHelper::deleteFriend($profile['id'], $friend);
+
+                    }
+
+                }
+
+                if ($photo = Photo::find()->where(['user_id' => $profile['id'] ] )->asArray()->all() ){
+
+                    foreach ($photo as $item){
+
+                        if (\file_exists($path.'/web'.$item['file'])) unlink($path.'/web'.$item['file']);
+
+                    }
+
+                    Photo::deleteAll(['user_id' =>$profile['id'] ] );
+
+                }
+
+                $filterParams = FilterParams::find()->asArray()->all();
+
+
+                foreach ($filterParams as $filterParam){
+
+                    if (!empty($filterParam['relation_class']) and !empty($filterParam['class_name'])) $filterParam['relation_class']::deleteAll('user_id = '.$profile['id']);
+
+                }
+
             }
 
         }
