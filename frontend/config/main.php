b855b14fac8322aaf7f41c15982c093c57aa5d54
sanya <sanyapodolski@gmail.com>
2020-04-24 10:47:26 +0300

    авторизация через вк

--- frontend/config/main.php
+++ frontend/config/main.php
@@ -77,6 +77,7 @@ return [
             'rules' => [
                 '<protocol>://<city:[a-z-0-9]+>.<domain>/' => 'site/index',
                 '<protocol>://<city:[a-z-0-9]+>.<domain>/auth' => 'site/auth',
+                '<protocol>://<city:[a-z-0-9]+>.<domain>/site/auth' => 'site/auth',
                 '<protocol>://<city:[a-z-0-9]+>.<domain>/cust' => 'site/cust',
                 '<protocol>://<city:[a-z-0-9]+>.<domain>/feedback' => 'site/feed-back',
                 '<protocol>://<city:[a-z-0-9]+>.<domain>/novosti' => 'news/list',
