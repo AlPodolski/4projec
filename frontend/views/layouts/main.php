f673025379cc1c51706a59ea7bd8dcd11c1da2c9
sanya <sanyapodolski@gmail.com>
2020-04-27 10:01:42 +0300

    авторизация через вк

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -196,12 +196,12 @@ $login = new LoginForm();
 
                 <?php echo LoginWidget::widget(); ?>
 
-                <div class="" style="display: none">
+
                     <?= yii\authclient\widgets\AuthChoice::widget([
                         'baseAuthUrl' => ['/auth'],
                         'popupMode' => false,
                     ]) ?>
-                </div>
+
 
             </div>
         </div>
