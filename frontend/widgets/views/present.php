89280e2bf9c5df470d3292240692025ea48ca942
sanya <sanyapodolski@gmail.com>
2020-05-14 16:07:20 +0300

    енивая загрузка подарков

--- frontend/widgets/views/present.php
+++ frontend/widgets/views/present.php
@@ -5,7 +5,7 @@
     <?php foreach ($data as $item) : ?>
 
         <div class=" present-item present-btn" onclick="get_present_form(this)" data-user-id="" data-present-id="<?php echo $item['id'] ?>">
-            <img src="<?php echo $item['img'] ?>" alt="">
+            <img loading="lazy" src="<?php echo $item['img'] ?>" alt="">
             <span class="present-name">
                 <?php echo $item['price'] ?> рублей
             </span>
