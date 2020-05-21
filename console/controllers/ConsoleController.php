6d723eae780e8636155221cf564409af9a509ab5
sanya <sanyapodolski@gmail.com>
2020-05-21 09:58:29 +0300

    удаление пользователей происходит через вхождения в имени

--- console/controllers/ConsoleController.php
+++ console/controllers/ConsoleController.php
@@ -60,124 +60,61 @@ class ConsoleController extends Controller
 
         $path = \str_replace('console', 'frontend',Yii::getAlias('@app') );
 
-        $names = ['Познакомлюсь', 'Хочу брать в рот', 'Групповой секс', 'Подрочу для женщин', 'Дрочу для девушек' ,
-            'Подрочу для вас',  'Москва-Домодедово',
-            'Переписка,видео,фото вирт',
-            'Виртуальный секс',
-            'Вирт-модель',
-            'Вирт с танюсей!',
-            'Вирт по whatsApp',
-            'Вирт WhatsApp Viber',
-            'Вирт WhatsApp Vibee',
-            'Вирт (не коммерция)',
-            'Вирт',
-            'Бина Сквирт',
-            'Пассив ищу актива',
-            'Ищу я с  местом',
-            'Ищу секс!!!',
-            'Ищу работу с интимом',
-            'Ищу пасика девочку',
-            'Ищу пару МЖ.  ЖЖ',
-            'Ищу пару мж,девушку',
-            'Ищу партнёршу',
-            'Ищу парня',
-            'Ищу мужчину',
-            'Ищу любовницу',
-            'Ищу женщину',
-            'Ищу друзей',
-            'Ищу друга за 50',
-            'Ищу друга',
-            'Ищу девушку!!!',
-            'Ищу девушку от 30',
-            'Ищу девушку ,женщину',
-            'Ищу девушку',
-            'Ищу в ЮЗАО',
-            'Ищу активного уни!',
-            'Ищу актива на минет',
-            'Ищу актива в Сочи',
-            'Ищу актива',
-            'Ищу акта.',
-            'Ищу',
-            'Ледибой',
-            'Тина  Сексуаль',
-            'Страпон секс',
-            'Сексуальная',
-            'Секси Аня',
-            'Секси',
-            'Секс и знакомство)',
-            'Секс втроем',
-            'Секс ватсап / вайбер',
-            'Секс в Люберцах',
-            'Секс',
-            'Пасссив хочет секса',
-            'Не коммерческий секс',
-            'Марина Секси',
-            'Марина жду на секс.',
-            'Люблю оральный секс.',
-            'Ищу секс!!!',
-            'Ишу секс',
-            'Групповой секс',
-            'Виртуальный секс',
-            'Випсекс',
-            'Стоячий член',
-            'Такси за минет',
-            'Сделаю минет.',
-            'Сделаю минет',
-            'Роза Звезда Минета',
-            'Минет/анал',
-            'Минет от любаши',
-            'Минет',
-            'Ищу актива на минет',
-            'Горячий минет',
-            'Глубокий минет',
-            'Виоллета минет',
-            'Отсосу, дам в попу',
-            'Отсосу взрослому',
-            'Отсосу',
-            'нужен молодой акт',
-            'Никому Ненужен',
-            'Актива ищу в Москве'];
+        $names = ['Групповой', 'секс', 'Глубокий',
+            'Пасс', 'Взаимные', 'Встречаемся',
+            'Начинающая', 'Виртуальный', 'Сделаю',
+            'Помогу', 'Попробую', 'Мастурбация', 'Шалю',
+            'Кто', 'Страпон', 'Взаимная', 'Мне', 'Хочу',
+            'ара', 'Куни', 'Актив', 'Подрочу', 'Парень',
+            'Ледибой', 'Нижняя', 'Молодая', 'Virtik', 'virt',
+            'вирт', 'одинокие', 'Стоячий', 'По ', 'Би'];
 
-        $profiles = Profile::find()->asArray()->where([ 'in' , 'username' , $names])->asArray()->all();
+        foreach ($names as $nane){
 
-        foreach ($profiles as $profile){
+            if($profiles = Profile::find()->asArray()->where([ 'like' , 'username' , $nane])->asArray()->all()){
 
-                Wall::deleteAll(['from' => $profile['id']]);
+                foreach ($profiles as $profile){
 
-                if ($friends = \frontend\modules\user\components\Friends::getFriendsIds($profile['id'])){
+                    Wall::deleteAll(['from' => $profile['id']]);
 
-                    foreach ($friends as $friend){
+                    if ($friends = \frontend\modules\user\components\Friends::getFriendsIds($profile['id'])){
 
-                        FriendsHelper::deleteFriend($profile['id'], $friend);
+                        foreach ($friends as $friend){
+
+                            FriendsHelper::deleteFriend($profile['id'], $friend);
+
+                        }
 
                     }
 
-                }
+                    if ($photo = Photo::find()->where(['user_id' => $profile['id'] ] )->asArray()->all() ){
 
-                if ($photo = Photo::find()->where(['user_id' => $profile['id'] ] )->asArray()->all() ){
+                        foreach ($photo as $item){
 
-                    foreach ($photo as $item){
+                            if (isset ($item['file']) and !empty($item['file']) and \file_exists($path.'/web'.$item['file'])) unlink($path.'/web'.$item['file']);
 
-                        if (isset ($item['file']) and !empty($item['file']) and \file_exists($path.'/web'.$item['file'])) unlink($path.'/web'.$item['file']);
+                        }
+
+                        Photo::deleteAll(['user_id' =>$profile['id'] ] );
 
                     }
 
-                    Photo::deleteAll(['user_id' =>$profile['id'] ] );
+                    $filterParams = FilterParams::find()->asArray()->all();
+
+
+                    foreach ($filterParams as $filterParam){
+
+                        if (!empty($filterParam['relation_class']) and !empty($filterParam['class_name'])) $filterParam['relation_class']::deleteAll('user_id = '.$profile['id']);
+
+                    }
+
+                    Profile::deleteAll('id = '.$profile['id']);
 
                 }
-
-                $filterParams = FilterParams::find()->asArray()->all();
-
-
-                foreach ($filterParams as $filterParam){
-
-                    if (!empty($filterParam['relation_class']) and !empty($filterParam['class_name'])) $filterParam['relation_class']::deleteAll('user_id = '.$profile['id']);
-
-                }
-
-                Profile::deleteAll('id = '.$profile['id']);
+            }
 
         }
+
     }
 
     public function actionDns(){
