47b6cfec30f90953e639b82dcd03f106b4078878
sanya <sanyapodolski@gmail.com>
2020-06-08 16:38:50 +0300

    равки с версткой переписки

--- frontend/controllers/SiteController.php
+++ frontend/controllers/SiteController.php
@@ -168,36 +168,18 @@ class SiteController extends Controller
 
     public function actionCust(){
 
-        $emails = array();
-
-        $from_time = \time() - 3600;
-
-        $to_time = \time() - 1800;
-
-        $user_ids = Message::find()->where(['status' => 0])->andWhere([ '>', 'created_at' , $from_time])
-            ->andWhere(['<', 'created_at' , $to_time])->groupBy('chat_id')->with('dialog')->all();
-
-        foreach ($user_ids as $user_id){
-
-            if (isset($user_id['dialog']['authorNoPhoto']['email']) and $user_id['dialog']['authorNoPhoto']['email']
-                and !\in_array( $user_id['dialog']['authorNoPhoto']['email'], $emails)){
-
-                $emails[] = $user_id['dialog']['authorNoPhoto']['email'];
-
                 Yii::$app
                     ->mailer
                     ->compose()
                     ->setFrom([Yii::$app->params['supportEmail'] => Yii::$app->name . ' '])
-                    ->setTo($user_id['dialog']['authorNoPhoto']['email'])
+                    ->setTo('test-yompuzm5h@srv1.mail-tester.com')
                     ->setSubject('Новое сообщений ' . Yii::$app->name)
-                    ->setHtmlBody('Здравствуйте '.$user_id['dialog']['authorNoPhoto']['username'].' , у Вас новое сообщение '
-                        .' <a href="https://'.$user_id['dialog']['authorNoPhoto']['city'].'.'.Yii::$app->params['site_name'].'/user/chat/">На сайте '. Yii::$app->name.'</a>')
-                    ->setTextBody('Здравствуйте '.$user_id['dialog']['authorNoPhoto']['username'].' , у Вас новое сообщение '
+                    ->setHtmlBody('Здравствуйте, у Вас новое сообщение '
+                        .' <a href="https://user/chat/">На сайте '. Yii::$app->name.'</a>')
+                    ->setTextBody('Здравствуйте , у Вас новое сообщение '
                         .' На сайте '. Yii::$app->name.'')
                     ->send();
 
-            }
-
         }
-    }
+
 }
