f088075d1f7463e8fe2e487e2b839c6c6f369d00
sanya <sanyapodolski@gmail.com>
2020-06-18 16:19:23 +0300

    добавлен подсчет комментариев

--- frontend/modules/advert/views/advert/view.php
+++ frontend/modules/advert/views/advert/view.php
@@ -13,7 +13,6 @@ $commentsForm = new \frontend\models\forms\AddCommentForm();
 $this->registerMetaTag([
         'name' => 'description',
         'content' =>  mb_substr($advert['text'], 0, 255),
-
 ]);
 
 ?>
@@ -80,6 +79,14 @@ $this->registerMetaTag([
                         <?php echo $advert['text']; ?>
 
                 </div>
+            </div>
+
+            <div class="comment-count-block">
+
+                <svg width="24" height="20" viewBox="0 0 24 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M21.8906 0.203125H2.10938C0.946289 0.203125 0 1.14941 0 2.3125V13.5625C0 14.7256 0.946289 15.6719 2.10938 15.6719H4.26562V19.1875C4.26562 19.7689 4.93048 20.0952 5.39062 19.75L10.8281 15.6719H21.8906C23.0537 15.6719 24 14.7256 24 13.5625V2.3125C24 1.14941 23.0537 0.203125 21.8906 0.203125ZM22.5938 13.5625C22.5938 13.9501 22.2783 14.2656 21.8906 14.2656H10.5938C10.4416 14.2656 10.2936 14.3151 10.1719 14.4062L5.67188 17.7812V14.9688C5.67188 14.5804 5.35712 14.2656 4.96875 14.2656H2.10938C1.72174 14.2656 1.40625 13.9501 1.40625 13.5625V2.3125C1.40625 1.92487 1.72174 1.60938 2.10938 1.60938H21.8906C22.2783 1.60938 22.5938 1.92487 22.5938 2.3125V13.5625Z" fill="#486BEF"/>
+                </svg>
+                <?php if (!empty($advert['comments'])) echo count($advert['comments']) ; else echo 0 ?>
             </div>
 
             <div class="comments-list">
