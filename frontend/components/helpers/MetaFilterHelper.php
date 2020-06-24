896514c05fc4c66fab1a86b6454078c2f5ad7e76
sanya <sanyapodolski@gmail.com>
2020-06-24 15:19:39 +0300

    равка с мета фильтром

--- frontend/components/helpers/MetaFilterHelper.php
+++ frontend/components/helpers/MetaFilterHelper.php
@@ -8,7 +8,7 @@ class MetaFilterHelper
 {
     public static function Filter($string)
     {
-        if (\strstr(\mb_strtolower($string), 'мужчин') or \strstr(\mb_strtolower($string), 'женщи') ){
+        if (\strstr(\mb_strtolower($string), 'мужчин') or \strstr(\mb_strtolower($string), 'женщи') or \strstr(\mb_strtolower($string), 'ользовател')){
             return $string;
         }
 
