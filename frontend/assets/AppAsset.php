1ef89f6a67cad9444296d799620f905edadb1ca7
sanya <sanyapodolski@gmail.com>
2020-07-02 16:26:38 +0300

    правка с шрифтами

--- frontend/assets/AppAsset.php
+++ frontend/assets/AppAsset.php
@@ -12,12 +12,12 @@ class AppAsset extends AssetBundle
     public $basePath = '@webroot';
     public $baseUrl = '@web';
     public $css = [
-        'css/site.css',
+        'css/site.cs?v=1s',
         'css/fonts.css?v=1',
         'files/fonts/muller/mullernarrow.css',
     ];
     public $js = [
-        'files/js/script.js'
+        'files/js/script.js?v=1'
     ];
     public $depends = [
         'yii\web\YiiAsset',
