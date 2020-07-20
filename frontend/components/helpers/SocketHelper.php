d9d99747ec9b077b262080fdf37717cbc7581457
sanya <sanyapodolski@gmail.com>
2020-07-20 15:48:18 +0300

    отправление подарков из админки

--- frontend/components/helpers/SocketHelper.php
+++ frontend/components/helpers/SocketHelper.php
@@ -10,21 +10,12 @@ class SocketHelper
 {
     public static function send_notification(array $params){
 
-        Yii::$app->params['send_params'] = $params;
+        \Ratchet\Client\connect(Yii::$app->params['websoket_addr'])->then(function($conn) {
 
-        $socket = @fsockopen(Yii::$app->params['websoket_addr_with_not_port_and_protocol'], Yii::$app->params['websoket_post'], $errno, $errstr, 5);
-
-        if ($socket){
-
-            \Ratchet\Client\connect(Yii::$app->params['websoket_addr'])->then(function($conn) {
-
-                $conn->send(\json_encode(Yii::$app->params['send_params']));
-
+            $conn->send(\json_encode(Yii::$app->params['send_params']));
             }, function ($e) {
                 echo "Could not connect: {$e->getMessage()}\n";
-            });
-
-        }
-
+        });
     }
+
 }
