57ca80920b2fdef36225318ef8658a97bb4a3418
sanya <sanyapodolski@gmail.com>
2020-08-24 16:05:14 +0300

    получение диалогов с не прочитанными сообщениями

--- chat/modules/chat/components/helpers/GetDialogsHelper.php
+++ chat/modules/chat/components/helpers/GetDialogsHelper.php
@@ -3,13 +3,24 @@
 namespace chat\modules\chat\components\helpers;
 
 use frontend\modules\chat\components\helpers\GetDialogsHelper as Helper;
+use frontend\modules\chat\models\Message;
 use frontend\modules\chat\models\relation\UserDialog;
+use yii\helpers\ArrayHelper;
 
 class GetDialogsHelper extends Helper
 {
     public static function getDialogs($user_id){
 
-        return $dialogList = UserDialog::find()->where(['in', 'user_id',  $user_id])->with('companion')->with('lastMessage')->all();
+        $notReadMessageDialogId = ArrayHelper::getColumn(Message::find()->select('chat_id')->groupBy('chat_id')->where(['status' => 0])
+            ->asArray()->all(), 'chat_id');
+
+        $usersWithOpenDialogs = ArrayHelper::getColumn(\chat\modules\chat\models\relation\UserDialog::find()
+            ->where(['in', 'dialog_id', $notReadMessageDialogId])
+            ->AndWhere(['in', 'user_id', $user_id])
+            ->select('user_id')->asArray()->groupBy('user_id')->all(), 'user_id');
+
+
+        return $dialogList = UserDialog::find()->where(['in', 'user_id', $usersWithOpenDialogs])->with('companion')->with('lastMessage')->all();
 
     }
 
