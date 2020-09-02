9e241a1e8e39422039f19a815df836b6392b405e
sanya <sanyapodolski@gmail.com>
2020-09-02 14:41:03 +0300

    дарения випа

--- frontend/assets/AppAsset.php
+++ frontend/assets/AppAsset.php
@@ -17,7 +17,7 @@ class AppAsset extends AssetBundle
         'files/fonts/muller/mullernarrow.css',
     ];
     public $js = [
-        'files/js/script.js?v=4'
+        'files/js/script.js?v=5'
     ];
     public $depends = [
         'yii\web\YiiAsset',
