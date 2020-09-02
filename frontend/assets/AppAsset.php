f0ec094d3aca770a7a0f1a6478b4e96a28e2578a
sanya <sanyapodolski@gmail.com>
2020-09-02 14:38:57 +0300

    дарения випа

--- frontend/assets/AppAsset.php
+++ frontend/assets/AppAsset.php
@@ -12,7 +12,7 @@ class AppAsset extends AssetBundle
     public $basePath = '@webroot';
     public $baseUrl = '@web';
     public $css = [
-        'css/site.css?v=4',
+        'css/site.css?v=5',
         'css/fonts.css?v=2',
         'files/fonts/muller/mullernarrow.css',
     ];
