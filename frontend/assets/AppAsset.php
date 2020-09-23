69d1b3908d893b459244fec2048acfb4bdde3ef3
sanya <sanyapodolski@gmail.com>
2020-09-23 16:04:00 +0300

    добавлена иконка закрыть фильтр

--- frontend/assets/AppAsset.php
+++ frontend/assets/AppAsset.php
@@ -15,7 +15,7 @@ class AppAsset extends AssetBundle
         'css/site.css?v=32',
     ];
     public $js = [
-        'files/js/script.js?v=18'
+        'files/js/script.js?v=19'
     ];
     public $depends = [
         'yii\web\YiiAsset',
