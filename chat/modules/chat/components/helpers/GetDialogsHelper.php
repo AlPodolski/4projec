cc3c2fc04f62d9d7e3ffa1466d33e2cf770d8931
sanya <sanyapodolski@gmail.com>
2020-10-08 16:44:24 +0300

    правки в работе чатов

--- chat/modules/chat/components/helpers/GetDialogsHelper.php
+++ chat/modules/chat/components/helpers/GetDialogsHelper.php
@@ -2,14 +2,24 @@
 
 namespace chat\modules\chat\components\helpers;
 
+use chat\modules\chat\models\Message;
 use frontend\modules\chat\components\helpers\GetDialogsHelper as Helper;
 use frontend\modules\chat\models\relation\UserDialog;
+use yii\helpers\ArrayHelper;
 
 class GetDialogsHelper extends Helper
 {
     public static function getDialogs($user_id){
 
-        return $dialogList = UserDialog::find()->where(['in', 'user_id',  $user_id])->orderBy('id DESC')->with('companion')->with('lastMessage')->all();
+        $notReadMessageDialogsIds = Message::find()->where(['status' => 0])->select('chat_id')->groupBy('chat_id')->asArray()->all();
+
+        $notReadMessageDialogsIds = ArrayHelper::getColumn($notReadMessageDialogsIds, 'chat_id');
+
+        return $dialogList = UserDialog::find()->where(['in', 'user_id',  $user_id])
+            ->andWhere(['in', 'dialog_id', $notReadMessageDialogsIds])
+            ->orderBy('id DESC')->with('companion')
+            ->with('lastMessage')
+            ->all();
 
     }
 
