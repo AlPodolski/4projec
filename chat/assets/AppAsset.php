1ff36f8cf623868b0989c16789c52eb4c6facd3b
sanya <sanyapodolski@gmail.com>
2020-10-09 09:03:23 +0300

    показываются прочитанные диалоги в чатах

--- chat/assets/AppAsset.php
+++ chat/assets/AppAsset.php
@@ -12,10 +12,10 @@ class AppAsset extends AssetBundle
     public $basePath = '@webroot';
     public $baseUrl = '@web';
     public $css = [
-        'css/site.css?v=8',
+        'css/site.css?v=9',
     ];
     public $js = [
-        'files/js/script.js?v=3'
+        'files/js/script.js?v=4'
     ];
     public $depends = [
         'yii\web\YiiAsset',
