5efcdb20f9d438485bd78db96cb1609048135020
sanya <sanyapodolski@gmail.com>
2020-10-15 14:28:52 +0300

    изменение работы зазывающего сообщения

--- frontend/views/anket/anket.php
+++ frontend/views/anket/anket.php
@@ -26,7 +26,7 @@ $this->registerCssFile('/css/owl.theme.default.min.css');
 
 SlickAsset::register($this);
 
-$this->registerJsFile('/files/js/single.js?v=10', ['depends' => [SlickAsset::className()]]);
+$this->registerJsFile('/files/js/single.js?v=11', ['depends' => [SlickAsset::className()]]);
 
 $photo = Photo::getUserphoto($model->id);
 $params = Params::find()->asArray()->all();
