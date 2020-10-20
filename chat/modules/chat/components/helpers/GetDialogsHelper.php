b03284ef0c2ff0c5b9f316c618c3804452826a1a
sanya <sanyapodolski@gmail.com>
2020-10-20 12:34:00 +0300

    правки с чатом

--- chat/modules/chat/components/helpers/GetDialogsHelper.php
+++ chat/modules/chat/components/helpers/GetDialogsHelper.php
@@ -11,7 +11,7 @@ class GetDialogsHelper extends Helper
 {
     public static function getDialogs($user_id){
 
-        $notReadMessageDialogsIds = Message::find()->where(['status' => 0])->select('chat_id')->groupBy('chat_id')->asArray()->all();
+        $notReadMessageDialogsIds = Message::find()->where(['status' => 0])->andWhere(['type' => 1])->select('chat_id')->groupBy('chat_id')->asArray()->all();
 
         $notReadMessageDialogsIds = ArrayHelper::getColumn($notReadMessageDialogsIds, 'chat_id');
 
