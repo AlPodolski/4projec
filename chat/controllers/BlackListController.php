088d7b0c21be80a3bff9f55d89a8b83c6a6b6799
sanya <sanyapodolski@gmail.com>
2021-02-18 14:31:34 +0200

    если пользователь что то покупал его нельзя добавить в чс

--- chat/controllers/BlackListController.php
+++ chat/controllers/BlackListController.php
@@ -6,6 +6,7 @@ namespace chat\controllers;
 use common\models\BlackList;
 use common\models\Presents;
 use frontend\models\forms\BuyPresentForm;
+use frontend\modules\user\models\Profile;
 use Yii;
 use yii\web\Controller;
 
@@ -17,7 +18,9 @@ class BlackListController extends Controller
 
             $userId = Yii::$app->request->post('user_id');
 
-            if (!BlackList::find()->where(['user_id' => $userId])->one()){
+            $profile = Profile::find()->where(['id' => $userId])->asArray()->all();
+
+            if ($profile['sum'] <= 0 and !BlackList::find()->where(['user_id' => $userId])->one()){
 
                 $blackList = new BlackList;
 
