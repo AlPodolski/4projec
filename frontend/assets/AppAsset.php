02da50fe78e95e087a6812585425d08ca5b23b41
sanya <sanyapodolski@gmail.com>
2021-02-22 14:21:46 +0200

    Изменена гадерея

--- frontend/assets/AppAsset.php
+++ frontend/assets/AppAsset.php
@@ -12,7 +12,7 @@ class AppAsset extends AssetBundle
     public $basePath = '@webroot';
     public $baseUrl = '@web';
     public $css = [
-        'css/site.css?v=50',
+        'css/site.css?v=51',
     ];
     public $js = [
         'files/js/script.js?v=24'
